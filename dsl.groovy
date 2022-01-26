71c7ee7226c8eba9cac855ef636421bf900bda60
root <root@ip-172-31-37-116.ap-south-1.compute.internal>
2022-01-26 12:03:11 +0000

    discard gitscm1

--- dsl.groovy
+++ dsl.groovy
@@ -1,4 +1,4 @@
-job('example-'${BUILD_MUMBER}){
+job('example-1'){
    description('dsl-job number one')
    logRotator {
         numToKeep(2)
@@ -21,3 +21,27 @@ job('example-'${BUILD_MUMBER}){
       shell('echo "Hello World!!!!"')
 }
 }
+job('example-2'){
+   description('dsl-job number one')
+   logRotator {
+        numToKeep(2)
+      }
+   scm{
+      git {
+        branch('master')
+        remote{
+         name('origin')
+         github('gitcloudvipin/mygitrepo' ,'https')
+         credentials('git-id')
+      }
+}
+
+   }
+  triggers{
+      githubPush()
+    }
+   steps{
+      shell('echo "Hello World!!!!"')
+}
+}
+
