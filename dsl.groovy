633ede12eec57477172b9c256fd72f71eedb1192
root <root@ip-172-31-37-116.ap-south-1.compute.internal>
2022-01-26 10:20:03 +0000

    discard gitscm1

--- dsl.groovy
+++ dsl.groovy
@@ -5,7 +5,7 @@ job('example'){
       }
    scm{
       github('https://github.com/gitcloudvipin/mygitrepo.git' ,'master')
-      {credentialsId('git-id')}
+      credentials('git-id')
       
    }
   triggers{
