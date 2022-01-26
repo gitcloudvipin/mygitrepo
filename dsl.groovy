e9308850d4b596822718db1e701aab0e4a62524b
root <root@ip-172-31-37-116.ap-south-1.compute.internal>
2022-01-26 08:58:34 +0000

    discard old build123

--- dsl.groovy
+++ dsl.groovy
@@ -1,7 +1,10 @@
 job('example'){
    description('dsl-job number one')
-   discardOldBuilds(int daysToKeep = -1, int numToKeep = -2, int artifactDaysToKeep = -1, int artifactNumToKeep = -1)
-
+   publishers {
+        publishBuild {
+            discardOldBuilds(7, 10)
+        }
+    } 
    steps{
       shell('echo "Hello World!!!!"')
 }
